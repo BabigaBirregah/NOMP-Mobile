06676fee7edb9c0737aa00492591c917da3ae71d
yipeng <huang.ypeng@gmail.com>
2014-06-12 22:23:51 +0200

    add auto-update for types, move error handling in UI thread, #3.

--- src/fr/utt/isi/nomp_mobile/config/Config.java
+++ src/fr/utt/isi/nomp_mobile/config/Config.java
@@ -3,6 +3,19 @@ package fr.utt.isi.nomp_mobile.config;
 public class Config {
 	
 	public static final String NOMP_API_ROOT = "http://saphir.utt.fr:3000/";
+	
+	// interval (on days) for update of the classifications&actor types via API
+	public static final int NOMP_API_UPDATE_INTERVAL = 30;
+	
+	/* Shared preferences: pref names and pref keys */
+	// names
+	public static final String PREF_NAME_CLASSIFICATION = "pref_classification";
+	public static final String PREF_NAME_ACTOR_TYPE = "pref_actor_type";
+	
+	// keys
+	public static final String PREF_KEY_TYPE_IS_UPDATED = "is_updated";
+	public static final String PREF_KEY_TYPE_UPDATED_AT = "updated_at";
+	/* ******************************************************** */
 
 	private Config() {
 	}
