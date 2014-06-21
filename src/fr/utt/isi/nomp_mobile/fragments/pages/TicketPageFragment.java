bebf3eda7077e967cd6d6ff17da560f4bdf6a1bc
yipeng <huang.ypeng@gmail.com>
2014-06-21 22:14:59 +0200

    Revert "update ticket page view, #15"
    This reverts commit ea7140e5d15828f76f90833fd0bf47ed38da5dda.

--- src/fr/utt/isi/nomp_mobile/fragments/pages/TicketPageFragment.java
+++ src/fr/utt/isi/nomp_mobile/fragments/pages/TicketPageFragment.java
@@ -1,8 +1,6 @@
 package fr.utt.isi.nomp_mobile.fragments.pages;
 
 import fr.utt.isi.nomp_mobile.R;
-import fr.utt.isi.nomp_mobile.models.ActorType;
-import fr.utt.isi.nomp_mobile.models.Classification;
 import fr.utt.isi.nomp_mobile.models.Ticket;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
@@ -36,71 +34,31 @@ public abstract class TicketPageFragment extends Fragment {
 
 		if (ticket != null) {
 			// set the views
-			// name
 			TextView nameView = (TextView) view.findViewById(R.id.ticket_title);
 			nameView.setText(ticket.getName());
-
-			// classification
-			Classification childClassification = (Classification) (new Classification(
-					getActivity()).retrieve(ticket.getClassification()));
-			String classificationLabel = childClassification.getName();
-			if (!childClassification.isParent()
-					&& childClassification.getParent() != null) {
-				Classification parentClassification = (Classification) (new Classification(
-						getActivity())
-						.retrieve(childClassification.getParent()));
-				classificationLabel = parentClassification.getName() + " > "
-						+ classificationLabel;
-			}
-			TextView classificationNameView = (TextView) view
-					.findViewById(R.id.ticket_classification_name);
-			classificationNameView.setText(classificationLabel);
-
-			// source actor type
-			ActorType childSource = (ActorType) (new ActorType(getActivity())
-					.retrieve(ticket.getSourceActorType()));
-			String sourceLabel = childSource.getName();
-			if (!childSource.isParent() && childSource.getParent() != null) {
-				ActorType parentSource = (ActorType) (new ActorType(
-						getActivity()).retrieve(childSource.getParent()));
-				sourceLabel = parentSource.getName() + " > " + sourceLabel;
-			}
-			TextView sourceActorTypeNameView = (TextView) view
-					.findViewById(R.id.ticket_source_actor_type_name);
-			sourceActorTypeNameView.setText(sourceLabel);
-
-			// target actor type
-			ActorType childTarget = (ActorType) (new ActorType(getActivity())
-					.retrieve(ticket.getTargetActorType()));
-			String targetLabel = childTarget.getName();
-			if (!childTarget.isParent() && childTarget.getParent() != null) {
-				ActorType parentTarget = (ActorType) (new ActorType(
-						getActivity()).retrieve(childTarget.getParent()));
-				targetLabel = parentTarget.getName() + " > " + targetLabel;
-			}
-			TextView targetActorTypeNameView = (TextView) view
-					.findViewById(R.id.ticket_target_actor_type_name);
-			targetActorTypeNameView.setText(targetLabel);
-
-			// location (address)
-			TextView locationView = (TextView) view
-					.findViewById(R.id.ticket_location);
+			
+			TextView classificationNameView = (TextView) view.findViewById(R.id.ticket_classification_name);
+			classificationNameView.setText(ticket.getClassificationName());
+			
+			TextView sourceActorTypeNameView = (TextView) view.findViewById(R.id.ticket_source_actor_type_name);
+			sourceActorTypeNameView.setText(ticket.getSourceActorTypeName());
+			
+			TextView targetActorTypeNameView = (TextView) view.findViewById(R.id.ticket_target_actor_type_name);
+			targetActorTypeNameView.setText(ticket.getTargetActorTypeName());
+			
+			TextView locationView = (TextView) view.findViewById(R.id.ticket_location);
 			locationView.setText(ticket.getAddress()); // TODO: integrate map
-
-			// from to
-			TextView periodView = (TextView) view
-					.findViewById(R.id.ticket_period);
-			periodView.setText(ticket.getStartDate() + " -> "
-					+ ticket.getEndDate());
-
-			// quantity
-			TextView quantityView = (TextView) view
-					.findViewById(R.id.ticket_quantity);
+			
+			TextView startDateView = (TextView) view.findViewById(R.id.ticket_start_date);
+			startDateView.setText(ticket.getStartDate());
+			
+			TextView endDateView = (TextView) view.findViewById(R.id.ticket_end_date);
+			endDateView.setText(ticket.getEndDate());
+			
+			TextView quantityView = (TextView) view.findViewById(R.id.ticket_quantity);
 			quantityView.setText("" + ticket.getQuantity());
-
-			// description
-			TextView descriptionView = (TextView) view
-					.findViewById(R.id.ticket_description);
+			
+			TextView descriptionView = (TextView) view.findViewById(R.id.ticket_description);
 			descriptionView.setText(ticket.getDescription());
 		}
 
